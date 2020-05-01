5f0f5cf9f66841a087cf57c39f0efa3ef7f3dbfa
张宜钊 <@DESKTOP-BP1HF9R>
2020-05-01 21:02:50 +0800

    用户微服务

---
+++ sp03-userservice/src/test/java/com/tedu/sp03/Sp03UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedu.sp03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp03UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
